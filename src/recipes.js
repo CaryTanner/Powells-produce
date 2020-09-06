37dd95fbc2340418a3506a2c949c1d731662cf4a
CaryTanner <cary.landon@gmail.com>
2020-09-06 09:05:59 +0200

    Add image as space save  and change prop name
    Prop nam e birdtype changed due to change in theme to produce.
    Added image to space saver as recipe is own page now

--- src/recipes.js
+++ src/recipes.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import RecipeCard from './recipeCard.js'
 const Recipes = (props) => {
-        if (props.birdType !== "") {
+        if (props.recipeSearchQuery !== "") {
                return ( <div>
                         <h2>Recipes</h2>
                         <div id="display-recipe">
-                            <RecipeCard addItemToCart={props.addItemToCart} closeRecipe={props.closeRecipe} recipeIsOpen={props.recipeIsOpen} openRecipe={props.openRecipe} recipeTitle={props.recipeTitles[0]} imgSrc={props.imgUrls[0]} recipeId={props.recipeIds[0]} />
+
+{/* create a way to loop through recipe results?? */}
+
+                            <RecipeCard 
+                                addItemToCart={props.addItemToCart} 
+                                closeRecipe={props.closeRecipe} 
+                                recipeIsOpen={props.recipeIsOpen} 
+                                openRecipe={props.openRecipe} 
+                                recipeTitle={props.recipeTitles[0]} 
+                                imgSrc={props.imgUrls[0]} 
+                                recipeId={props.recipeIds[0]} 
+                            />
                             
-                            <RecipeCard addItemToCart={props.addItemToCart} closeRecipe={props.closeRecipe} recipeIsOpen={props.recipeIsOpen} openRecipe={props.openRecipe} recipeTitle={props.recipeTitles[1]} imgSrc={props.imgUrls[1]} recipeId={props.recipeIds[1]} />
+                            <RecipeCard 
+                                addItemToCart={props.addItemToCart} 
+                                closeRecipe={props.closeRecipe} 
+                                recipeIsOpen={props.recipeIsOpen} 
+                                openRecipe={props.openRecipe} 
+                                recipeTitle={props.recipeTitles[1]} 
+                                imgSrc={props.imgUrls[1]} 
+                                recipeId={props.recipeIds[1]} 
+                            />
                             
                             {/* <RecipeCard addItemToCart={props.addItemToCart} closeRecipe={props.closeRecipe} recipeIsOpen={props.recipeIsOpen} openRecipe={props.openRecipe} recipeTitle={props.recipeTitles[2]} imgSrc={props.imgUrls[2]} recipeId={props.recipeIds[2]} />
                             <RecipeCard addItemToCart={props.addItemToCart} closeRecipe={props.closeRecipe} recipeIsOpen={props.recipeIsOpen} openRecipe={props.openRecipe} recipeTitle={props.recipeTitles[3]} imgSrc={props.imgUrls[3]} recipeId={props.recipeIds[3]} />
@@ -16,7 +35,10 @@ const Recipes = (props) => {
                     </div>
                     </div>)
                     
-        } else return <div id="choose-bird-space-saver"> Choose your favorite bird...</div>
+        } else return <div id="choose-recipe-space-saver"> 
+                            <h2>Recipes</h2> 
+                            <img src="https://images.pexels.com/photos/1194432/pexels-photo-1194432.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img> 
+                        </div>
       
       
     }
