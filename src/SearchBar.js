023ee9c3647ac831a687d68cfe561d595c0870d6
CaryTanner <cary.landon@gmail.com>
2020-09-09 05:11:12 +0200

    add autocomplete

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,29 +1,35 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-
+import { fetchAutoComplete } from './api'
 
 const SearchBar = (props) => {
 
     const [searchQuery, setSearchQuery] = useState('');
+    const [suggestions, setSuggestion] = useState('');
     
-    // const handleEnter = (event) => {
-    //     if (event.key === 'Enter') {
-    //         console.log('entered')
-    //        this.props.searchQuery()
-    //     }
-    // }
-
+    useEffect(()=>{
+        
+        fetchAutoComplete(searchQuery)
+        .then(resp => setSuggestion(resp))
+        .catch(err => console.log(err)) 
+        
+    }, [searchQuery])
+console.log(suggestions)
     return <div>
         
         <input id="search-bar-input" 
                 data-searchquery={searchQuery} 
-                // onKeyDown={event => { if (event.key === 'enter'){props.searchQuery}}}
+
+                //--keydown doesn't redirect to recipes page from other??
+                onKeyDown={(event) => event.keyCode == 13 ? props.searchQuery(event) : ''}
                 onChange={() => {
                     setSearchQuery(document.querySelector('#search-bar-input').value)}} 
-                type="text" placeholder="Get inspired by 1000's of recipes..."
+                type="text" placeholder=" &#128269; Get inspired by 1000's of recipes..."
         />
-                    
+
+ {/* doesn't disappear when on new page        */}
+         { suggestions && <ul>{suggestions.map(suggestion => <Link to={"/recipes/"+ suggestion.id} ><li>{suggestion.title}</li></Link>)}</ul> }        
         <Link to="/recipes">     
         <button id="search-button" 
                 data-searchquery={searchQuery} 
@@ -38,4 +44,3 @@ const SearchBar = (props) => {
 
 export default SearchBar
 
-// document.querySelector('#search-bar-input')
