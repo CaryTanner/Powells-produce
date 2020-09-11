433f63c0c2676a2551b18bb4c44bcc78e7f1e83e
CaryTanner <cary.landon@gmail.com>
2020-09-11 15:10:16 +0200

    remove dead code

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -5,21 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export const ingredientList = (ing, clickFunc) => (
-     <li data-ingredient-id={ing.id}  cartItem={ing.name.charAt(0).toUpperCase() + ing.name.slice(1)} key={ing.name}> 
-    {ing.name.charAt(0).toUpperCase() + ing.name.slice(1)} {Math.ceil(ing.measures.metric.amount)} {ing.measures.metric.unitShort} 
+     <li data-ingredient-id={ing.id}  cartItem={ing.name.charAt(0).toUpperCase() + ing.name.slice(1)} key={ing.name} > 
+             {ing.name.charAt(0).toUpperCase() + ing.name.slice(1)} {Math.ceil(ing.measures.metric.amount)} {ing.measures.metric.unitShort} 
         <span title="Add to Cart"  class="shopping-cart-icon" onClick={clickFunc}>
             <FontAwesomeIcon icon="shopping-cart"/>
         </span>
     </li>
     );
 
-    export const ingredientListRemove = (ing, clickFunc) => (
- 
-           <span cartItem={ing.innerText} title="Remove Item"  class="shopping-cart-icon" onClick={clickFunc}>
-              {ing} x
-           </span>
-       
-       );
    
 
     
