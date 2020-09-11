b0d3acb4f8286c7684cf849bd2226ab2277267d5
CaryTanner <cary.landon@gmail.com>
2020-09-11 15:11:00 +0200

    change placement for mobile & shallow copy final cart items

--- src/orderConfirmation.js
+++ src/orderConfirmation.js
@@ -3,16 +3,25 @@ import React from 'react'
 
 const OrderConfirmation = (props) => { 
             
-             return (<div>
-                  <h2>Thanks for ordering!</h2>
-                  <h3>Order Details:</h3>
-                    <h4>First Name: {props.firstName}</h4>  
-                    <h4>Last Name: {props.lastName}</h4> 
-                    <h4>Address: {props.address}</h4>
-                    <h4>Delivery Time: {props.deliveryTime}</h4>  
-                    <h4>Delivery Date: {props.deliveryDate}</h4>     
-                    <ul>{props.cartItems}</ul>
-            </div>) 
+             return (
+                <div>
+                    <h2>Thanks for ordering!</h2>
+                    <div id="order-confirmation">
+                        <div>
+                            <h3 className="border-bottom">Order Details</h3>
+                            <h4>First Name: {props.firstName}</h4>  
+                            <h4>Last Name: {props.lastName}</h4> 
+                            <h4>Address: {props.address}</h4>
+                            <h4>Delivery Time: {props.deliveryTime}</h4>  
+                            <h4>Delivery Date: {props.deliveryDate}</h4>                                 
+                        </div>
+                     
+                        <div>
+                            <h3 className="border-bottom"> Items Ordered </h3>
+                            <ul>{props.orderedItems && props.orderedItems.map(x => <li>{x}</li>)}</ul>
+                        </div> 
+                    </div>
+                </div>) 
 
 
     
