bbb7f423f1946097af766a0ef889a8bfc5e3b172
CaryTanner <cary.landon@gmail.com>
2020-09-30 14:28:27 +0200

    Updates

---
+++ precache-manifest.1a6d44c7817594cdc362bbe5d76a564b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fe193ad567d0e3cb98161e3e0dd9b3",
+    "url": "/carytanner.com/powells-produce/index.html"
+  },
+  {
+    "revision": "b22078e6ee1845e37406",
+    "url": "/carytanner.com/powells-produce/static/css/main.68ed57eb.chunk.css"
+  },
+  {
+    "revision": "88a4c5a2e31c3e654bc1",
+    "url": "/carytanner.com/powells-produce/static/js/2.95f074e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/carytanner.com/powells-produce/static/js/2.95f074e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22078e6ee1845e37406",
+    "url": "/carytanner.com/powells-produce/static/js/main.af92e37f.chunk.js"
+  },
+  {
+    "revision": "abc6274f9710b8ab0588",
+    "url": "/carytanner.com/powells-produce/static/js/runtime-main.a3f11e3b.js"
+  }
+]);
